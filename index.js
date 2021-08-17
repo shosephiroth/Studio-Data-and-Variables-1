48e8635b1e8ab5f4393f22e2841b3952b2b508ab
Matthew Harris <44537080+Mattx2k1@users.noreply.github.com>
2021-08-17 11:41:23 +0000

    Created variables

--- index.js
+++ index.js
@@ -2,7 +2,18 @@
 
 
 // Declare and initialize the 12 variables here:
-
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg * fuelMassKg + shuttleMassKg;
+let fuelTempCelsius = -225;
+let fuelLevel = "100%";
+let weatherStatus = "clear";
 
 
 
